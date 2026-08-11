2cf66191cfe89e6ac909ddcc473fe0587aa68bee
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-11 19:27:31 +0530

    Fix header global type in Header component

--- src/Header/Component.tsx
+++ src/Header/Component.tsx
@@ -3,7 +3,8 @@ import { getCachedGlobal } from '@/utilities/getGlobals'
 import React from 'react'
 
 export async function Header() {
-  const headerData = await getCachedGlobal('header', 1)()
+  // 'header' என்பதுடன் as any சேர்த்து டைப் பிழையைத் தவிர்க்கவும்
+  const headerData = await getCachedGlobal('header' as any, 1)()
 
   return <HeaderClient data={headerData} />
 }
