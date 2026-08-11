77c7f91fd45ba42a44cd5d8ce49887393a0dd2d4
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-11 22:24:42 +0530

    Fix updates

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260811_165340 from './20260811_165340';
+
+export const migrations = [
+  {
+    up: migration_20260811_165340.up,
+    down: migration_20260811_165340.down,
+    name: '20260811_165340'
+  },
+];
