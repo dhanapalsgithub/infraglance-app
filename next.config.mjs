fd773cc754ec5df851db4cc9a7661cae45633337
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-11 22:59:31 +0530

    Fix updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { redirects } from './redirects'
 
 const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
 
-// பாதுகாப்பான முறையில் URL-ஐ கையாளுதல் (Invalid URL பிழையைத் தவிர்க்க)
 const getServerUrl = () => {
   if (process.env.NEXT_PUBLIC_SERVER_URL) return process.env.NEXT_PUBLIC_SERVER_URL
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
@@ -17,7 +15,6 @@ const getServerUrl = () => {
 const NEXT_PUBLIC_SERVER_URL = getServerUrl()
 
 const nextConfig = {
-  // Vercel-ல் sharp சரியாகச் செயல்பட இது அவசியம்
   serverExternalPackages: ['sharp', '@payloadcms/db-postgres'],
 
   sassOptions: {
@@ -51,7 +48,6 @@ const nextConfig = {
     return webpackConfig
   },
   reactStrictMode: true,
-  redirects,
   turbopack: {
     root: path.resolve(dirname),
   },
