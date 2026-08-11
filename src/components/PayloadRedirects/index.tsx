a447e4c2d1692de58d59b8daf18c6f0b7e0d6cae
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-11 17:50:31 +0530

    Fix typescript relationTo comparison error in PayloadRedirects

--- src/components/PayloadRedirects/index.tsx
+++ src/components/PayloadRedirects/index.tsx
@@ -1,5 +1,5 @@
 import type React from 'react'
-import type { Page, Post } from '@/payload-types'
+import type { Config, Page, Post } from '@/payload-types'
 
 import { getCachedDocument } from '@/utilities/getDocument'
 import { getCachedRedirects } from '@/utilities/getRedirects'
@@ -26,7 +26,7 @@ export const PayloadRedirects: React.FC<Props> = async ({ disableNotFound, url }
     const relationTo = redirectItem.to?.reference?.relationTo as string
 
     if (typeof redirectItem.to?.reference?.value === 'string') {
-      const collection = relationTo
+      const collection = relationTo as keyof Config['collections']
       const id = redirectItem.to?.reference?.value
 
       const document = (await getCachedDocument(collection, id)()) as Page | Post
@@ -47,5 +47,5 @@ export const PayloadRedirects: React.FC<Props> = async ({ disableNotFound, url }
 
   if (disableNotFound) return null
 
-  notfound()
+  notFound()
 }
