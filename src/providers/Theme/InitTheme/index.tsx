4d6d87f310151617428aac76e4aed101be8add89
dhanapalsgithub <dhanapaul2020@gmail.com>
2026-08-11 17:39:00 +0530

    Add packageManager and Next.js dependencies for Vercel

--- src/providers/Theme/InitTheme/index.tsx
+++ src/providers/Theme/InitTheme/index.tsx
@@ -1,50 +1,48 @@
-import Script from 'next/script'
+'use client'
+import { useServerInsertedHTML } from 'next/navigation'
 import React from 'react'
 
 import { defaultTheme, themeLocalStorageKey } from '../ThemeSelector/types'
 
 export const InitTheme: React.FC = () => {
-  return (
-    // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
-    <Script
-      dangerouslySetInnerHTML={{
-        __html: `
-  (function () {
-    function getImplicitPreference() {
-      var mediaQuery = '(prefers-color-scheme: dark)'
-      var mql = window.matchMedia(mediaQuery)
-      var hasImplicitPreference = typeof mql.matches === 'boolean'
+  useServerInsertedHTML(() => {
+    const scriptContent = `
+      (function () {
+        function getImplicitPreference() {
+          var mediaQuery = '(prefers-color-scheme: dark)'
+          var mql = window.matchMedia(mediaQuery)
+          var hasImplicitPreference = typeof mql.matches === 'boolean'
 
-      if (hasImplicitPreference) {
-        return mql.matches ? 'dark' : 'light'
-      }
+          if (hasImplicitPreference) {
+            return mql.matches ? 'dark' : 'light'
+          }
 
-      return null
-    }
+          return null
+        }
 
-    function themeIsValid(theme) {
-      return theme === 'light' || theme === 'dark'
-    }
+        function themeIsValid(theme) {
+          return theme === 'light' || theme === 'dark'
+        }
 
-    var themeToSet = '${defaultTheme}'
-    var preference = window.localStorage.getItem('${themeLocalStorageKey}')
+        var themeToSet = '${defaultTheme}'
+        var preference = window.localStorage.getItem('${themeLocalStorageKey}')
 
-    if (themeIsValid(preference)) {
-      themeToSet = preference
-    } else {
-      var implicitPreference = getImplicitPreference()
+        if (themeIsValid(preference)) {
+          themeToSet = preference
+        } else {
+          var implicitPreference = getImplicitPreference()
 
-      if (implicitPreference) {
-        themeToSet = implicitPreference
-      }
-    }
+          if (implicitPreference) {
+            themeToSet = implicitPreference
+          }
+        }
 
-    document.documentElement.setAttribute('data-theme', themeToSet)
-  })();
-  `,
-      }}
-      id="theme-script"
-      strategy="beforeInteractive"
-    />
-  )
+        document.documentElement.setAttribute('data-theme', themeToSet)
+      })();
+    `
+
+    return <script dangerouslySetInnerHTML={{ __html: scriptContent }} id="theme-script" />
+  })
+
+  return null
 }
